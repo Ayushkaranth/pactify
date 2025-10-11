f69dd8d848b7f423814e0ad97d0cd476ca3b63dc
Ayush A Karanth <ayushkaranth2005@gmail.com>
2025-10-11 22:22:15 +0530

    updated file

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@
 "use client";
 
 import React from "react";
-// ❌ Remove this import
-// import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -11,12 +9,6 @@ import { Web3Provider } from "@/providers/Web3Provider";
 import { ShootingStarBackground } from "@/components/background-animation";
 
 const inter = Inter({ subsets: ["latin"] });
-
-// ❌ Remove this export as it is now in app/metadata.ts
-// export const metadata: Metadata = {
-//   title: "Pactify",
-//   description: "Verifiable Commitments",
-// };
 
 export default function RootLayout({
   children,
